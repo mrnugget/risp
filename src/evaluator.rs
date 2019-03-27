3a58274fc507853bd24f02c5302727bed892f4e6
Thorsten Ball <mrnugget@gmail.com>
2019-03-27 07:08:46 +0100

    Make builtin functions work!

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
 use crate::object;
-use crate::object::Object;
 use crate::object::Function;
+use crate::object::Object;
 
 pub struct Environment {
     entries: HashMap<String, Object>,
@@ -14,8 +14,14 @@ impl Environment {
             entries: HashMap::new(),
         };
 
-        env.define(String::from("+"), Object::Callable(Function::Native(object::sum)));
-        env.define(String::from("*"), Object::Callable(Function::Native(object::multiply)));
+        env.define(
+            String::from("+"),
+            Object::Callable(Function::Native(object::sum)),
+        );
+        env.define(
+            String::from("*"),
+            Object::Callable(Function::Native(object::multiply)),
+        );
 
         env
     }
@@ -30,32 +36,37 @@ impl Environment {
         }
     }
 
-    fn get(&self, key: &String) -> &Object {
+    fn get(&self, key: &String) -> Object {
         match self.entries.get(key) {
             Some(val) => val.clone(),
-            None => &Object::Nil,
+            None => Object::Nil,
         }
     }
 }
 
-// pub fn apply(proc: &Object, args: Object) -> Object {
-//     if let Object::Builtin(func) = proc {
-//         return func(args);
-//     }
-//
-//     Object::Nil
-// }
+pub fn apply(proc: &Object, args: &[Object]) -> Object {
+    if let Object::Callable(func) = proc {
+        println!("here we are!");
+        let Function::Native(builtin) = func;
+        return builtin(args);
+    }
 
-pub fn eval<'a>(exp: &'a Object, env: &'a Environment) -> &'a Object {
+    // TODO: this should be an error
+    Object::Nil
+}
+
+pub fn eval<'a>(exp: Object, env: &'a Environment) -> Object {
     match exp {
         Object::Nil | Object::Integer(_) | Object::Callable(_) => exp,
 
-        Object::Symbol(name) => env.get(name),
-        Object::List(ref elems) => &elems[0],
-        // Object::List(ref elems) => {
-        //     let proc = eval(&elems[0], env);
-        //     &apply(proc, &elems[1..elems.len()-1])
-        // }
+        Object::Symbol(name) => env.get(&name),
+        Object::List(elems) => {
+            let mut iter = elems.into_iter();
+            let first = iter.next().unwrap();
+            let args = iter.collect::<Vec<Object>>();
+            let proc = eval(first, env);
+            apply(&proc, &args)
+        }
     }
 }
 
@@ -69,23 +80,23 @@ mod tests {
         ( $input:expr, $expected:expr ) => {{
             let objects = reader::read($input).unwrap();
             assert_eq!(objects.len(), 1);
-            let exp = objects.first().unwrap();
+            let exp = objects.into_iter().next().unwrap();
 
             let env = Environment::new();
-            let result = eval(exp.clone(), &env);
+            let result = eval(exp, &env);
             assert_eq!(result, $expected);
         }};
     }
 
     #[test]
     fn test_self_evaluating() {
-        assert_eval!("15", &Object::Integer(15));
+        assert_eval!("15", Object::Integer(15));
     }
 
     #[test]
     fn test_eval_builtins() {
-        assert_eval!("(+ 1 2 3)", &Object::Integer(6));
-        assert_eval!("(+ 1 2 3 4 5 6)", &Object::Integer(21));
-        assert_eval!("(* 2 2 2 2)", &Object::Integer(16));
+        assert_eval!("(+ 1 2 3)", Object::Integer(6));
+        assert_eval!("(+ 1 2 3 4 5 6)", Object::Integer(21));
+        assert_eval!("(* 2 2 2 2)", Object::Integer(16));
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -28,7 +28,7 @@ impl Clone for Function {
     }
 }
 
-#[derive(PartialEq)]
+#[derive(Clone, PartialEq)]
 pub enum Object {
     Nil,
     Integer(i64),
@@ -96,39 +96,15 @@ impl fmt::Debug for Object {
     }
 }
 
-// pub fn cons(car: Rc<Object>, cdr: Rc<Object>) -> Rc<Object> {
-//     Rc::new(Object::Pair(car.clone(), cdr.clone()))
-// }
-//
-// pub fn car(pair: Rc<Object>) -> Rc<Object> {
-//     match pair.deref() {
-//         Object::Pair(car, _) => car.clone(),
-//         _ => Rc::new(Object::Nil),
-//     }
-// }
-//
-// pub fn cdr(pair: Rc<Object>) -> Rc<Object> {
-//     match pair.deref() {
-//         Object::Pair(_, cdr) => cdr.clone(),
-//         _ => Rc::new(Object::Nil),
-//     }
-// }
-//
-// pub fn cadr(pair: Rc<Object>) -> Rc<Object> {
-//     car(cdr(pair))
-// }
-//
-// pub fn caadr(pair: Rc<Object>) -> Rc<Object> {
-//     car(car(cdr(pair)))
-// }
-
 // TODO: This can merged with `multiply` into a generic `fold_integers`
 pub fn sum(args: &[Object]) -> Object {
     let mut sum = 0;
     for i in args.iter() {
         if let Object::Integer(val) = i {
+            println!("val={}", val);
             sum += val;
         } else {
+            println!("nil?");
             return Object::Nil;
         }
     }
@@ -173,33 +149,4 @@ mod tests {
         let multiply_result = multiply(&args);
         assert_eq!(multiply_result, Object::Integer(6));
     }
-
-    // #[test]
-    // fn test_consing_objects() {
-    //     let four = Rc::new(Object::Integer(4));
-    //     let five = Rc::new(Object::Integer(5));
-    //
-    //     let pair = cons(four.clone(), five.clone());
-    //     assert!(pair.is_pair());
-    //     if let Object::Pair(car, cdr) = pair.deref() {
-    //         assert_eq!(car.deref(), four.deref());
-    //         assert_eq!(cdr.deref(), five.deref());
-    //     }
-    //
-    //     assert_eq!(car(pair.clone()).deref(), four.deref());
-    //     assert_eq!(cdr(pair).deref(), five.deref());
-    // }
-
-    // #[test]
-    // fn dereferencing() {
-    //     let pair = Rc::new(Object::Pair(
-    //         Rc::new(Object::Integer(1)),
-    //         Rc::new(Object::Nil),
-    //     ));
-    //
-    //     assert_eq!(*car(pair.clone()), Object::Integer(1));
-    //
-    //     let number = Rc::new(Object::Integer(4));
-    //     assert_eq!(*number, Object::Integer(4));
-    // }
 }
